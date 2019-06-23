fcec0c91a336ee0b7c884294c0cce50c81736504
Andreas Reinle <gras64@web.de>
2019-06-23 23:53:31 +0200

    Update corpus_file_gen.py

--- corpus_file_gen.py
+++ corpus_file_gen.py
@@ -15,8 +15,8 @@
 import re
 import argparse
 import wikipedia as wiki
-from lingua_franca.format import pronounce_number, nice_date, nice_time, nice_response_de
-from lingua_franca.parse import extract_numbers, normalize
+from lingua_franca/lingua_franca.format import pronounce_number, nice_date, nice_time, nice_response_de
+from lingua_franca/lingua_franca.parse import extract_numbers, normalize
 #import requests
 
 
